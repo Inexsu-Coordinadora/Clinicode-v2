55833f6bc99dca46531cc4e2b04f9e57bf9e718f
Jader Florez <florezlopezjaderandres@gmail.com>
2025-11-08 22:25:32 -0500

    Versión base funcional del módulo de pacientes

---
+++ src/presentacion/app.ts
@@ -0,0 +1,15 @@
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import { pacienteEnrutador } from './rutas/pacienteEnrutador.js';
+
+
+const app = Fastify({ logger: true });
+
+app.register(cors);
+app.register(pacienteEnrutador, { prefix: '/api' });
+
+app.get('/', async () => {
+    return { mensaje: 'Servidor Fastify funcionando' };
+});
+
+export default app;
